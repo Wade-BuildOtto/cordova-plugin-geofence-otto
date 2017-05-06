c1c4b3c8b69d9fd175c27cacee1e83bd4f706800
Colin Humber <colinhumber@gmail.com>
2017-05-05 18:20:54 -0600

    Added AppDelegate category with launch override
    The category swizzles -application:didFinishLaunchingWithOptions: to handle when the app launches fresh due to a location change. This is swizzled to remove the need to create a custom AppDelegate class. However, if the Cordova-generated UIApplicationDelegate class is not named AppDelegate, this won't work and will likely cause build issues.

---
+++ src/ios/AppDelegate+LocationStartup.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+LocationStartup.h
+//  GeoFencev2
+//
+//  Created by Colin Humber on 2017-05-05.
+//
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (LocationStartup)
+
+@end
